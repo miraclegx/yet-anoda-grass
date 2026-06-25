54d5e1e22cfe9d82d47c50de0ca8c3784ce3b5a1
miracle iyanu <miracleojonugwamaji@gmail.com>
2026-06-25 14:01:06 +0100

    got instancing working with 4 instatiated grass models

---
+++ main/grass.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "model"
+  type: "model"
+  data: "mesh: \"/main/grass.glb\"\n"
+  "name: \"{{NAME}}\"\n"
+  "materials {\n"
+  "  name: \"default\"\n"
+  "  material: \"/main/my_instaced.material\"\n"
+  "}\n"
+  ""
+}
